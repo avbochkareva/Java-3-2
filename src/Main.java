8a56d0e4d02af1faef76844e44d9eca6dc49dfae
avbochkareva <81125737+avbochkareva@users.noreply.github.com>
2021-04-19 20:23:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите начальную сумму на балансе: ");
-        long num = in.nextInt();
-        System.out.print("Сколько рублей положить на баланс? ");
-        long num2 = in.nextInt();
-                long sum = num + num2;
-        if (sum >= 1000 )
+        long startСoins = 100; //начальный баланс 100 рублей
+        System.out.print("Баланс: 100 рублей. Сколько рублей положить на баланс? ");
+        long addСoins = in.nextInt();
+        if (addСoins > 1000)
         {
-            long sum2 = sum / 100 - 1;
-            System.out.printf("Вам начислено: " + sum2 + " бонусов. " +
-                    "Итого на счету: " + (sum2 + sum) + " рублей");
+            long bonus = addСoins / 100;
+            System.out.println("Вам начислено: " + bonus + " бонусов. " +
+                    "Баланс: " + (bonus + addСoins + startСoins) + " рублей");
         }
         else
-            System.out.println("Вам начислено 0 бонусов. Итого на счету: " + sum + " рублей");
+            System.out.println("Вам начислено 0 бонусов. Итого на счету: " + (addСoins + startСoins) + " рублей");
     }
 }
